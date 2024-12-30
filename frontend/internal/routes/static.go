164d5c2a83aa3d5dd874aeeea4169eb687f8de5c
leminhnguyenai <leminhnguyenai@gmail.com>
2024-12-30 10:51:22 +0700

    Add custom font

--- frontend/internal/routes/static.go
+++ frontend/internal/routes/static.go
@@ -10,10 +10,15 @@ func serveCSS(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/output.css")
 }
 
+func serveFont(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/font/JetBrainsMono.ttf")
+}
+
 func StaticRouter() *api.Router {
 	router := api.NewRouter()
 
 	router.GET("/output.css", http.HandlerFunc(serveCSS))
+	router.GET("/jet-brains-mono.tff", http.HandlerFunc(serveFont))
 
 	return router
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./templates/**/*.html'],
+    theme: {
+        fontFamily: {
+            mono: ['JetBrainsMono'],
+        },
+    },
     plugins: [],
 }
