3ca996d1302b1c5fee8efd23544cc911775d2512
leminhnguyenai <leminhnguyenai@gmail.com>
2025-01-11 11:26:53 +0700

    Shorten the code to create oauth2 config

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -8,14 +8,6 @@ import (
 	"golang.org/x/oauth2/google"
 )
 
-var Scopes = []string{
-	"https://www.googleapis.com/auth/userinfo.email",
-	"https://www.googleapis.com/auth/calendar",
-	"https://www.googleapis.com/auth/calendar.readonly",
-	"https://www.googleapis.com/auth/calendar.events",
-	"https://www.googleapis.com/auth/calendar.events.readonly",
-}
-
 const DbTimeout = time.Millisecond * 500
 
 const GoogleAPITimeout = time.Second * 10000
@@ -27,7 +19,13 @@ func Oauth2Config() *oauth2.Config {
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
 		RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
-		Scopes:       Scopes,
-		Endpoint:     google.Endpoint,
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/calendar",
+			"https://www.googleapis.com/auth/calendar.readonly",
+			"https://www.googleapis.com/auth/calendar.events",
+			"https://www.googleapis.com/auth/calendar.events.readonly",
+		},
+		Endpoint: google.Endpoint,
 	}
 }
