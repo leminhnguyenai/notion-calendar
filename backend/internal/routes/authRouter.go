9bf176ea96be272676f1c1e45fd65e34a3ac25e1
leminhnguyenai <leminhnguyenai@gmail.com>
2025-01-11 22:43:24 +0700

    case fixing

--- backend/internal/routes/authRouter.go
+++ backend/internal/routes/authRouter.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	. "github.com/leminhnguyenai/notion-calendar/backend/internal/controllers/authcontroller"
+	. "github.com/leminhnguyenai/notion-calendar/backend/internal/controller/authcontroller"
 	"github.com/leminhnguyenai/notion-calendar/backend/internal/helpers/api"
 	"github.com/leminhnguyenai/notion-calendar/backend/internal/middlewares"
 )
